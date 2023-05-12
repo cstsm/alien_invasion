ea9eed245d5e7a73396fa95b4c46f57753cb47be
cshuo <cshuof@qq.com>
2023-05-12 10:52:04 +0800

    the first

---
+++ alien_invasion.py
@@ -0,0 +1,51 @@
+import sys
+import pygame
+from settings import Settings
+from ship import Ship
+
+class AlienInvasion:
+    """管理游戏资源和行为的类"""
+    def __init__(self):
+        """初始化游戏并创建游戏资源"""
+        pygame.init()
+        self.settings = Settings()
+
+        #创建一个窗口，并设置标题
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width,self.settings.screen_height)
+        )
+        pygame.display.set_caption("Alien Invasion")
+
+        #飞船
+        self.ship = Ship(self)
+
+        #设置背景色。
+        self.bg_color = (self.settings.bg_color)
+
+    def run_game(self):
+        """开始游戏的主循环"""
+        while True:
+            #监视键盘和鼠标事件。
+            self._check_events()
+            #更新屏幕
+            self._update_screen()
+    
+    def _check_events(self):
+        """响应按键和鼠标事件。"""
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+    
+    def _update_screen(self):
+        """更新屏幕上的图像，并切换到新的屏幕"""
+        #每次循环时都重绘屏幕
+        self.screen.fill(self.bg_color)
+        self.ship.blitme()
+
+        #让最近绘制的屏幕可见。
+        pygame.display.flip()
+
+if __name__ == '__main__':
+    #创建游戏实例并运行游戏。
+    ai = AlienInvasion()
+    ai.run_game()
